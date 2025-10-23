e390773cda51a2042c2ccaa5d161a331e4055b10
ahmadnaseer143 <anaseer942@gmail.com>
2025-10-23 16:15:27 -0500

    scafolding user table

---
+++ OnlineShop/Models/Db/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnlineShop.Models.Db;
+
+public partial class User
+{
+    public int Id { get; set; }
+
+    public string FullName { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+
+    public bool IsAdmin { get; set; }
+
+    public DateTime? RegisterDate { get; set; }
+
+    public int? RecoveryCode { get; set; }
+}
