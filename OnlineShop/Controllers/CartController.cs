a37ffe3e049032edb5cd88c09ceee9e2f21784ba
ahmadnaseer143 <anaseer942@gmail.com>
2025-11-06 11:38:38 -0600

    updating cart part 1

---
+++ OnlineShop/Controllers/CartController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Models.Db;
+
+namespace OnlineShop.Controllers
+{
+    public class CartController : Controller
+    {
+        private OnlineShopContext _context;
+        public CartController(OnlineShopContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
